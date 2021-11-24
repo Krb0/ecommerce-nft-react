ce0ea5507fc20dd9a505e6ac736f5f8dcf5caa13
Krb0 <87597855+Krb0@users.noreply.github.com>
2021-11-24 13:36:59 -0300

    404 page added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import HomeSection from "../components/Home/HomeSection";
 import styled from "styled-components";
+
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
