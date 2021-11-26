9c4b7092d2e443bc3dec21992ceaf79e9bea94d5
Krb0 <87597855+Krb0@users.noreply.github.com>
2021-11-26 02:43:44 -0300

    fixed item detail

--- src/components/Products/containers/ItemDetail.jsx
+++ src/components/Products/containers/ItemDetail.jsx
@@ -1,37 +1,38 @@
-import React from 'react'
-import profileImg from '../../../images/profile.png';
+import React from "react";
+import profileImg from "../../../images/profile.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEthereum } from "@fortawesome/free-brands-svg-icons";
-const ItemDetail = ({product}) => {
-    return (
-        <>
-            <div className="image-container" >
-          <img className="nft-image" src={product.nftimage} alt={'nft icon'} />
-        </div>
-        <div className="nft-info">
-          <h1>{product.name}
-          </h1>
-          <p className="nft-stock" >{product.stock} in stock</p>
-          <p className="nft-text">Lorem ipsum Enim id anim deserunt est irure irure voluptate dolor adipisicing occaecat pariatur. Irure nulla consequat ullamco ex ex consectetur laboris. Id consectetur esse commodo enim labore consectetur. Enim minim minim veniam.</p>
-          <div className="price-creator-container">
-            
-            <div className="price-container" >
-              <FontAwesomeIcon icon={faEthereum} />
-              <p>{product.price} ETH</p>
-            </div>
-            <a className="nft-creator-container" href="https://github.com/krb0">
-              <img src={profileImg} alt='profile icon' />
-              <h3>Krb0</h3>
-            </a>
-            
+const ItemDetail = ({ product }) => {
+  return (
+    <>
+      <div className="image-container">
+        <img className="nft-image" src={product.nftimage} alt={"nft icon"} />
+      </div>
+      <div className="nft-info">
+        <h1>{product.name}</h1>
+        <p className="nft-stock">{product.stock} in stock</p>
+        <p className="nft-text">
+          Lorem ipsum Enim id anim deserunt est irure irure voluptate dolor
+          adipisicing occaecat pariatur. Irure nulla consequat ullamco ex ex
+          consectetur laboris. Id consectetur esse commodo enim labore
+          consectetur. Enim minim minim veniam.
+        </p>
+        <div className="price-creator-container">
+          <div className="price-container">
+            <FontAwesomeIcon icon={faEthereum} />
+            <p>{product.price} ETH</p>
           </div>
-          <div class="btn-container">
-
+          <a className="nft-creator-container" href="https://github.com/krb0">
+            <img src={profileImg} alt="profile icon" />
+            <h3>Krb0</h3>
+          </a>
+        </div>
+        <div className="btn-container">
           <button className="btn-explore">Buy Now</button>
-          </div>
         </div>
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default ItemDetail
+export default ItemDetail;
