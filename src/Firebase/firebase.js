f32c9898a5d355d5eca2c86ffe6005504c719f4a
Krb <87597855+Krb0@users.noreply.github.com>
2021-12-31 03:35:38 -0300

    added .env

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -8,17 +8,18 @@ import {
   addDoc,
   doc,
 } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "creaturesnft-caceres.firebaseapp.com",
-  projectId: "creaturesnft-caceres",
-  storageBucket: "creaturesnft-caceres.appspot.com",
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
